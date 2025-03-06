7476510166054e80f58d9dd9ea42870037f0653c
Ethan denton <ethanjdenton1@gmail.com>
2025-03-06 16:17:46 +0000

    added nav function, cleaned up app.tsx

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,79 +1,6 @@
-import { useState } from "react";
 import "./App.css";
-import { useScrollValue } from "./hooks/useScrollValue";
-import { useWindowDimesions } from "./hooks/useWindowDimensions";
+import { Navbar } from "./navbar";
 
-function Navbar() {
-  const scrollposition = useScrollValue();
-  const { height, width } = useWindowDimesions();
-  const [active, setactive] = useState(Number);
-
-  return (
-    <>
-      {scrollposition < height ? (
-        <nav className="navhigh">
-          <div
-            className="navi1"
-            onClick={() => {
-              setactive(1);
-            }}
-            style={
-              active == 1
-                ? {
-                    filter: "drop-shadow(0 0 5px #3E5179)",
-                    boxShadow:
-                      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
-                  }
-                : { flex: 1 }
-            }
-          >
-            Projects
-          </div>
-          <div
-            className="navi2"
-            onClick={() => {
-              setactive(2);
-            }}
-            style={
-              active == 2
-                ? {
-                    filter: "drop-shadow(0 0 5px #3E5179)",
-                    boxShadow:
-                      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
-                  }
-                : { flex: 1 }
-            }
-          >
-            Home
-          </div>
-          <div
-            className="navi2"
-            onClick={() => {
-              setactive(3);
-            }}
-            style={
-              active == 3
-                ? {
-                    filter: "drop-shadow(0 0 5px #3E5179)",
-                    boxShadow:
-                      "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
-                  }
-                : { flex: 1 }
-            }
-          >
-            Contact Me
-          </div>
-        </nav>
-      ) : (
-        <nav className="navlow">
-          <div className="navilow1">Projects</div>
-          <div className="navilow2">Home</div>
-          <div className="navilow2">Contact Me</div>
-        </nav>
-      )}
-    </>
-  );
-}
 
 function Showcase() {
   return (
@@ -101,14 +28,24 @@ function Projects() {
         <div className="project-desc">
           <div className="project-title">[Title]</div>
           <div className="project-info">[long parahgraph]</div>
-          <div className="project-skills">
-            Skills I learnt:
-            <ul>
-              <li>Skill 1</li>
-              <li>Skill 2</li>
-              <li>Skill 3</li>
-              <li>Skill 4</li>
-            </ul>
+          <div className="project-list-cont">
+            <div className="project-skills">
+              Skills I learnt:
+              <ul>
+                <li>Skill 1</li>
+                <li>Skill 2</li>
+                <li>Skill 3</li>
+                <li>Skill 4</li>
+              </ul>
+            </div>
+            <div className="project-lang">
+              Languages used:
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
---
+++ Portfolio/src/navbar.tsx
@@ -0,0 +1,49 @@
+import { useScrollValue } from "./hooks/useScrollValue";
+import { useWindowDimesions } from "./hooks/useWindowDimensions";
+
+export const Navbar = () => {
+  const scrollposition = useScrollValue();
+  const { height } = useWindowDimesions();
+
+  return (
+    <>
+      {scrollposition < height ? (
+        <nav className="navhigh">
+          <div
+            className="navi1"
+            onClick={() => {
+              window.scrollTo({ top: height, behavior: "smooth" });
+            }}
+          >
+            Projects
+          </div>
+          <div
+            className="navi2"
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
+            Home
+          </div>
+          <div className="navi2">Contact Me</div>
+        </nav>
+      ) : (
+        <nav className="navlow">
+          <div
+            className="navilow1"
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            Home
+          </div>
+          <div
+            className="navilow2"
+            onClick={() => window.scrollTo({ top: height, behavior: "smooth" })}
+          >
+            Projects
+          </div>
+          <div className="navilow2">Contact Me</div>
+        </nav>
+      )}
+    </>
+  );
+};
